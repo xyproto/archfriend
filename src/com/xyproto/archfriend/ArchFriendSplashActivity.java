c17b04432b9a538408ef673bcccf042e77213d3f
Alexander Rødseth <rodseth@gmail.com>
2012-11-29 15:57:03 +0100

    Minor changes

--- src/com/xyproto/archfriend/ArchFriendSplashActivity.java
+++ src/com/xyproto/archfriend/ArchFriendSplashActivity.java
@@ -2,7 +2,7 @@
 
   MIT license:
 
-	Copyright (c) 2011 Alexander Rødseth
+	Copyright (c) 2012 Alexander Rødseth
 	
 	Permission is hereby granted, free of charge, to any person obtaining
 	a copy of this software and associated documentation files (the
